193f2c18a6a5806b6cfb02b9d719a68d004848f6
Alejandro <arivass23@fpsanturtzilh.eus>
2025-01-20 11:31:05 +0100

    editatu commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,6 +2,28 @@ import { Component } from '@angular/core';
 import { DataService } from '../services/data.service';
 import { AlertController } from '@ionic/angular';
 
+interface RootObject {
+  page: number;
+  per_page: number;
+  total: number;
+  total_pages: number;
+  data: Datum[];
+  support: Support;
+}
+
+interface Support {
+  url: string;
+  text: string;
+}
+
+interface Datum {
+  id: number;
+  email: string;
+  first_name: string;
+  last_name: string;
+  avatar: string;
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -9,6 +31,9 @@ import { AlertController } from '@ionic/angular';
   standalone: false,
 })
 export class HomePage {
+  auxemail!:string;
+  auxfirstname!:string;
+  auxlastname!:string;
 
   dataArray = [
     {
@@ -20,6 +45,8 @@ export class HomePage {
     },
     // Agrega más elementos según sea necesario
   ];
+
+
 
   constructor(private dataService: DataService, private alertController: AlertController) {}
 
@@ -36,9 +63,53 @@ export class HomePage {
     );
   }
 
-  addIdazle() {
-    console.log('Añadir idazle');
-    // Lógica para añadir un idazle (por ejemplo, abrir un modal o formulario)
+  async addIdazle() {
+    console.log('Sortu idazle');
+    const alert = await this.alertController.create({
+      header: 'Sortu Idazlea',
+      inputs: [
+        {
+          name: 'first_name',
+          type: 'text',
+          value: this.auxfirstname,
+          placeholder: 'Izena',
+        },
+        {
+          name: 'last_name',
+          type: 'text',
+          value: this.auxlastname,
+          placeholder: 'Abizena',
+        },
+        {
+          name: 'email',
+          type: 'email',
+          value: this.auxemail,
+          placeholder: 'Email',
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            console.log('Edición cancelada');
+          },
+        },
+        {
+          text: 'Gorde',
+          handler: (data) => {
+            // Actualizar los datos del idazle
+            this.auxfirstname = data.first_name;
+            this.auxlastname = data.last_name;
+            this.auxemail = data.email;
+            this.dataArray.push(data)
+            console.log('Idazle sortuta:');
+          },
+        },
+      ],
+    });
+
+    await alert.present();
   }
   
   async editIdazle(idazle: any) {
@@ -49,19 +120,19 @@ export class HomePage {
           name: 'first_name',
           type: 'text',
           value: idazle.first_name,
-          placeholder: 'Nombre',
+          placeholder: 'Izena',
         },
         {
           name: 'last_name',
           type: 'text',
           value: idazle.last_name,
-          placeholder: 'Apellido',
+          placeholder: 'Abizena',
         },
         {
           name: 'email',
           type: 'email',
           value: idazle.email,
-          placeholder: 'Correo Electrónico',
+          placeholder: 'Email',
         },
       ],
       buttons: [
